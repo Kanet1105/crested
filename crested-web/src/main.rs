54cf778bb160b62e11b84bd21962d96132ba08e9
Donghoon Lee <90014039+Kanet1105@users.noreply.github.com>
2022-06-13 17:47:47 +0900

    added an index page for the webcam

--- crested-web/src/main.rs
+++ crested-web/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, middleware, App, HttpRequest, HttpResponse, HttpServer, Result};
+use actix_web::http::StatusCode;
+
+#[get("/")]
+async fn index(req: HttpRequest) -> Result<HttpResponse> {
+    Ok(HttpResponse::build(StatusCode::OK)
+        .content_type("text/html; charset=utf-8")
+        .body(include_str!("./static/webcam.html")))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .wrap(middleware::Compress::default())
+            .service(index)
+    })
+    .bind(("127.0.0.1", 8000))?
+    .run()
+    .await
 }
