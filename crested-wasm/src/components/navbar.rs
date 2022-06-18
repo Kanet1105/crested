c86cbd61810567e3f38aa10990a6ec92f781e3df
Allen Clarence <zombiedelah@gmail.com>
2022-06-19 00:07:15 +0900

    changed the navigation bar syntax to fit the bootstrap 5 style

--- crested-wasm/src/components/navbar.rs
+++ crested-wasm/src/components/navbar.rs
@@ -12,13 +12,23 @@ impl Component for NavBar {
 
     fn view(&self, _ctx: &Context<Self>) -> Html {
         html! {
-            <nav id="global-navbar">
-                <div>
-                    <button><a>{ "menu 1" }</a></button>
-                </div>
-                    <button><a>{ "menu 2" }</a></button>
-                <div>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div class="container fw-bold">
+                    // the brand button
+                    <a class="navbar-brand" href="/">{"Crested"}</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
 
+                    // the actual navigation menu
+                    <div class="collapse navbar-collapse show" id="navbarNavAltMarkup">
+                        <div class="navbar-nav">
+                            <a class="nav-link" href="/">{"Home"}</a>
+                            <a class="nav-link" href="/">{"Features"}</a>
+                            <a class="nav-link" href="/">{"Pricing"}</a>
+                            <a class="nav-link" href="/">{"Disabled"}</a>
+                        </div>
+                    </div>
                 </div>
             </nav>
         }
