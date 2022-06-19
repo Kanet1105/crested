b0c3b10740ce7e1fb5a17cb0822d890301f9aa26
Donghoon Lee <90014039+Kanet1105@users.noreply.github.com>
2022-06-19 17:16:42 +0900

    a module folder where static pages are

---
+++ crested-wasm/src/pages/features.rs
@@ -0,0 +1,22 @@
+use yew::{Context, Component, Html, html};
+use crate::components::navbar::NavBar;
+
+pub struct Features;
+
+impl Component for Features {
+    type Message = ();
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self
+    }
+
+    fn view(&self, _ctx: &Context<Self>) -> Html {
+        html! {
+            <>
+            <header><NavBar/></header>
+            <div><NavBar/></div>
+            </>
+        }
+    }
+}
---
+++ crested-wasm/src/pages/home.rs
@@ -0,0 +1,21 @@
+use yew::{Context, Component, Html, html};
+use crate::components::navbar::NavBar;
+
+pub struct Home;
+
+impl Component for Home {
+    type Message = ();
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self
+    }
+
+    fn view(&self, _ctx: &Context<Self>) -> Html {
+        html! {
+            <>
+            <header><NavBar/></header>
+            </>
+        }
+    }
+}
