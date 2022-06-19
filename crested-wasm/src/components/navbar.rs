e5bbc9e9206fb1e0b00e8fc97fd3dc3946a0a830
Allen Clarence <zombiedelah@gmail.com>
2022-06-20 01:40:45 +0900

    added properties on the NavBar component (still under development)

--- crested-wasm/src/components/navbar.rs
+++ crested-wasm/src/components/navbar.rs
@@ -1,21 +1,28 @@
-use yew::{Context, Component, Html, html};
+use yew::{html, Children, Context, Component, Html, Properties, virtual_dom::AttrValue};
+
+#[derive(Properties, PartialEq)]
+pub struct Props {
+    pub nav_title: AttrValue,
+    #[prop_or_default]
+    pub nav_tabs: Children,
+}
 
 pub struct NavBar;
 
 impl Component for NavBar {
     type Message = ();
-    type Properties = ();
+    type Properties = Props;
 
     fn create(_ctx: &Context<Self>) -> Self {
         Self
     }
 
-    fn view(&self, _ctx: &Context<Self>) -> Html {
+    fn view(&self, ctx: &Context<Self>) -> Html {
         html! {
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div class="container fw-bold">
                     // the brand button
-                    <a class="navbar-brand" href="/">{"Crested"}</a>
+                    <a class="navbar-brand" href="/">{ ctx.props().nav_title.clone() }</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
