1f1818c3e757aa6380d160e5cb938760fc97790b
Allen Clarence <zombiedelah@gmail.com>
2022-06-23 17:01:48 +0900

    removed redundant directories after the renaming

--- crested-wasm/src/main.rs
+++
@@ -1,5 +0,0 @@
-use crested_wasm::App;
-
-fn main() {
-    yew::Renderer::<App>::new().render();
-}
--- crested-wasm/src/pages/developer.rs
+++
@@ -1,8 +0,0 @@
-use yew::prelude::*;
-
-#[function_component]
-pub fn Developer() -> Html {
-    html! {
-        "developer"
-    }
-}
