afac5e551ba0aa8d1f66815fb3f0bf02b42d910b
Donghoon Lee <90014039+Kanet1105@users.noreply.github.com>
2022-07-20 18:42:52 +0900

    [Enhancement] Add examples.

--- core/src/guides/interior_mutability.rs
+++ core/src/guides/interior_mutability.rs
@@ -32,3 +32,53 @@ fn example() {
 }
 
 // 위 example 에서 data: Vec<i32> 의 변화를 공유하도록 만들기.
+use std::cell::RefCell;
+use std::rc::Rc;
+
+#[test]
+pub fn example_1() {
+    let data = Rc::new(RefCell::new(vec![1, 2, 3, 4, 5]));
+    let component_1 = Rc::clone(&data);
+    let component_2 = data.clone();
+    dbg!(Rc::strong_count(&data));
+    {
+        let mut data_1 = data.borrow_mut();
+        data_1.push(6);
+    }
+    println!("data: {:?}", &data);
+    println!("component_1: {:?}", &component_1);
+    println!("component_2: {:?}", *component_2);
+}
+
+// Rc<RefCell<T>> 를 통해서 구현.
+#[derive(Debug)]
+struct SharedStruct {
+    data: RefCell<Vec<i32>>,
+}
+
+impl SharedStruct {
+    pub fn new(data: Vec<i32>) -> Rc<Self> {
+        Rc::new(Self { data: RefCell::new(data), })
+    }
+
+    pub fn push(&self, number: i32) {
+        let mut data = self.data.borrow_mut();
+        data.push(number);
+    }
+}
+
+#[test]
+fn example_2() {
+    let component_1 = SharedStruct::new(vec![1, 2, 3, 4, 5]);
+    let component_2 = Rc::clone(&component_1);
+    println!("{:?}", component_1);
+    println!("{:?}", component_2);
+
+    component_2.push(6);
+    println!("{:?}", component_1);
+    println!("{:?}", component_2);
+
+    component_1.push(7);
+    println!("{:?}", component_1);
+    println!("{:?}", component_2);
+}
