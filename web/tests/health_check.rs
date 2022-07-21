d084c89e960a2ee7d9727c9ec7699143182aa2a9
Allen Clarence <zombiedelah@gmail.com>
2022-07-21 22:49:56 +0900

    [Features] Add a health_check test code

--- web/tests/health_check.rs
+++ web/tests/health_check.rs
@@ -3,24 +3,27 @@ use std::net::TcpListener;
 #[tokio::test]
 async fn health_check() {
     // run the web server.
-    spawn_app();
+    let address = spawn_app();
     let client = reqwest::Client::new();
 
     // send a request.
     let response = client
-        .get("http://127.0.0.1:8080")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute the request.");
 
     // assertions
+    println!("{:?}", response);
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
 }
 
-async fn spawn_app() {
-    let listener = TcpListener::bind("127.0.0.1:8080")
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0")
         .expect("Failed to bind the port");
+    let port = listener.local_addr().unwrap().port();
     let server = web::run(listener).expect("Failed to bind the address.");
     let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
 }
