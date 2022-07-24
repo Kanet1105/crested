1ba2e0363780c377a5308c551fc824a8dad5629d
Donghoon Lee <90014039+Kanet1105@users.noreply.github.com>
2022-07-24 14:27:57 +0900

    [Enhancement] Add an example.

--- core/src/guides/error_handling.rs
+++ core/src/guides/error_handling.rs
@@ -1,20 +1,38 @@
 use core::num;
 /// 사용자 정의 에러 타입
 /// 내가 직접 에러를 만들고 싶을 때
-use std::error::Error;
 use std::fmt;
 
-struct EvenIntegerError {}
+struct EvenIntegerError;
 
+impl fmt::Display for EvenIntegerError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "The answer cannot be an even integer..")
+    }
+}
+
+impl fmt::Debug for EvenIntegerError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "The answer cannot be an even integer..")
+    }
+}
+
+impl std::error::Error for EvenIntegerError {}
+
+/// 반환하는 에러 타입이 하나만 존재할 때는 아래와 같이 구현 가능.
 #[test]
 fn example_1() {
     let numerator = 6;
-    let denominator = 3;
-    let answer = example_1_inner(&numerator, &denominator);
+    let denominator = 0;
+    let answer = example_1_inner(&numerator, &denominator).unwrap();
     dbg!(answer);
 }
 
-fn example_1_inner<'a>(numer: &'a i32, denom: &'a i32) -> i32 {
+fn example_1_inner<'a>(numer: &'a i32, denom: &'a i32) -> Result<i32, EvenIntegerError> {
     let answer: i32 = numer / denom;
-    answer
+    if answer % 2 == 0 {
+        Err(EvenIntegerError)
+    } else {
+        Ok(answer)
+    }
 }
