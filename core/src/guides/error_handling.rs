ec6c8cf82d742e9e915f4be9f010baa800ddd66b
Donghoon Lee <90014039+Kanet1105@users.noreply.github.com>
2022-07-24 14:45:10 +0900

    [Enhancement] Add an example_2.

--- core/src/guides/error_handling.rs
+++ core/src/guides/error_handling.rs
@@ -19,6 +19,22 @@ impl fmt::Debug for EvenIntegerError {
 
 impl std::error::Error for EvenIntegerError {}
 
+struct OneError;
+
+impl fmt::Display for OneError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "The answer cannot be 1..")
+    }
+}
+
+impl fmt::Debug for OneError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "The answer cannot be 1..")
+    }
+}
+
+impl std::error::Error for OneError {}
+
 /// 반환하는 에러 타입이 하나만 존재할 때는 아래와 같이 구현 가능.
 #[test]
 fn example_1() {
@@ -32,6 +48,26 @@ fn example_1_inner<'a>(numer: &'a i32, denom: &'a i32) -> Result<i32, EvenIntege
     let answer: i32 = numer / denom;
     if answer % 2 == 0 {
         Err(EvenIntegerError)
+    } else {
+        Ok(answer)
+    }
+}
+
+/// 하지만 반환해야 하는 에러 타입이 2 개가 넘을 경우 dynamic dispatch 로 해결 가능
+#[test]
+fn example_2() {
+    let numerator = 6;
+    let denominator = 6;
+    let answer = example_2_inner(&numerator, &denominator).unwrap();
+    dbg!(answer);
+}
+
+fn example_2_inner<'a>(numer: &'a i32, denom: &'a i32) -> Result<i32, &'a dyn std::error::Error> {
+    let answer: i32 = numer / denom;
+    if answer == 1 {
+        Err(&OneError)
+    } else if answer % 2 == 0 {
+        Err(&EvenIntegerError)
     } else {
         Ok(answer)
     }
